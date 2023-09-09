8a9fc4580a6f5031b9f963e032a87c42432f1751
Gabriel Dias <gabriel.diasramos@hotmail.com>
2023-09-09 02:05:26 -0300

    criada a logica dos botoes, do calculo do imc e da entrada de dados. APP ainda sem style

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
 import  {Text, TextInput, View, Button} from 'react-native'
-
+import ResultadoIMC from './ResultIMC/retorno'
 
 export default function Form() {
+    const [altura, setAltura] = useState(null)
+    const [peso, setPeso] = useState(null)
+    const [mensagem, setMensagem] = useState("Preencha o peso e altura")
+    const [imc, setIMC] = useState(null)
+    const[textButton, setTextButton] = useState("Calcular")
+
+    function calcularIMC() {
+        return setIMC((peso/(altura * altura)).toFixed(2))
+    }
+    function conferir() {
+        if(peso != null && altura != null) {
+            calcularIMC()
+            setAltura(null)
+            setPeso(null)
+            setMensagem("Seu IMC é igual a:")
+            setTextButton("Calcular Novamente")
+            return
+        } 
+        setIMC(null)
+        setTextButton("Calcular")
+        setMensagem("Preencha o peso e altura")
+    }
+
     return(
        <View>
         <View>
             <Text>Altura</Text>
             <TextInput
+            onChangeText={setAltura}
+            value={altura}
             placeholder='Digite sua altura - Ex: 1.75'
             keyboardType="numeric"
             />
 
             <Text>Peso</Text>
             <TextInput
+            onChangeText={setPeso}
+            value={peso}
             placeholder='Digite seu peso - Ex: 85'
             keyboardType='numeric'
             />
-            <Button title='Calcular IMC'/>
+            <Button 
+            onPress={() => conferir()}
+            title={textButton}/>
         </View>
-        <calcularIMC mensagemResultado = {mensagem} resultadoIMC = {imc}/>
+       <ResultadoIMC mensagemResultadoIMC = {mensagem} resultadoIMC = {imc}/> 
        </View>
     );
 }
